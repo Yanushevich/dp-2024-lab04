e5d240a8e1f619cb2e4c1a9970da61204b751fe4
Yanushevich <maksim_yanushevich@mail.ru>
2024-11-04 19:25:55 +0500

    feat(files): Написание документации

--- clocks/digital_clock_adapter.py
+++ clocks/digital_clock_adapter.py
@@ -4,14 +4,32 @@ from consts import DayNightDivision
 
 
 class DigitalClockAdapter(BaseDigitalClock):
+    """
+    Адаптер для работы с интерфейсом цифровых часов
+    """
     def __init__(self, analog_clock: BaseAnalogClock):
+        """
+        Инициализирует адаптер
+
+        :param analog_clock: объект интерфейса аналоговых часов
+        """
         self._analog_clock = analog_clock
         self.date = None
 
     def get_hour_angle(self) -> int:
+        """
+        Возвращает значение угла часовой стрелки, учитывая время в минутах
+
+        :return: возвращает угол положения часовой стрелки в формате int
+        """
         return 30 * (self.date.hour % 12) + self.date.minute * 0.5
 
     def get_hour(self) -> int:
+        """
+        Возвращает значение часа
+
+        :return: возвращает значение времени в часах в формате int
+        """
         return (
             int(self._analog_clock.get_hour_angle() // 30)
             if (self.get_day_night_division() == DayNightDivision.AM)
@@ -19,24 +37,54 @@ class DigitalClockAdapter(BaseDigitalClock):
         )
 
     def get_minute_angle(self) -> int:
+        """
+        Возвращает значение угла минутной стрелки, учитывая время в секундах
+
+        :return: возвращает угол положения минутной стрелки в формате int
+        """
         return 6 * self.date.minute + self.date.second * 0.1
 
     def get_minute(self) -> int:
+        """
+        Возвращает значение минуты
+
+        :return: возвращает значение времени в минутах в формате int
+        """
         return int(self._analog_clock.get_minute_angle() // 6)
 
     def get_second_angle(self) -> int:
+        """
+        Возвращает значение угла секундной стрелки
+
+        :return: возвращает угол положения секундной стрелки в формате int
+        """
         return 6 * self.date.second
 
     def get_second(self) -> int:
+        """
+        Возвращает значение секунды
+
+        :return: возвращает значение времени в секундах в формате int
+        """
         return int(self._analog_clock.get_second_angle() // 6)
 
     def get_day_night_division(self):
+        """
+        Возвращает время суток
+
+        :return: возвращает время суток в формате DayNightDivision
+        """
         if self.date.hour > 11:
             return DayNightDivision.PM
         else:
             return DayNightDivision.AM
 
     def set_date_time(self, date: datetime) -> None:
+        """
+        Устанавливает дату, сохраняя данные в аналоговом формате
+
+        :param date: дата в формате datetime
+        """
         self.date = date
         self._analog_clock.set_date_time(
             self.date.year,
@@ -49,6 +97,11 @@ class DigitalClockAdapter(BaseDigitalClock):
         )
 
     def get_date_time(self) -> datetime:
+        """
+        Получает дату, обращаясь к объекту, хранящему данные в аналоговом формате
+
+        :return: возвращает дату в формате datetime
+        """
         return datetime(
             self._analog_clock.get_year(),
             self._analog_clock.get_month(),
--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import clocks
 
 
 def main():
+    # Создаем объект для хранения даты в аналоговом формате
     analog_clock = clocks.AnalogClock()
+    # Создаем объект адаптера, передавая ему объект для хранения
     digital_clock = clocks.DigitalClockAdapter(analog_clock)
+    # Задаем дату в цифровом формате
     digital_clock.set_date_time(datetime(2024, 11, 4, 5, 59, 31))
+    # Проверим данные в цифровом формате
     print(digital_clock.get_date_time())
+    # Проверим данные в аналоговом формате
+    print(analog_clock.get_year(), analog_clock.get_month(), analog_clock.get_day())
     print(analog_clock.get_hour_angle())
     print(analog_clock.get_minute_angle())
     print(analog_clock.get_second_angle())
+    print(analog_clock.day_night_division)
 
 
 if __name__ == "__main__":
